c477293ce5b78a68fd25125477fda5f29a722772
simonageo <sag954@gmail.com>
2023-01-21 23:59:42 +0200

    The js-file for the console game.

---
+++ guessANumber.js
@@ -0,0 +1,29 @@
+function guessANumber() {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    let computerGuess = Math.floor(Math.random() * 100);
+    let recursiveAsyncReadLine = function () {
+        readline.question(`Guess the number (0-100): `, guess => {
+            if (guess >= 0 && guess <= 100) {
+                if (guess == computerGuess) {
+                    console.log(`You guess it!`);
+                    return readline.close();
+                } else if (guess < computerGuess) {
+                    console.log(`Too low!`);
+                    recursiveAsyncReadLine();
+                } else if (guess > computerGuess) {
+                    console.log(`Too High!`);
+                    recursiveAsyncReadLine();
+                }
+            } else {
+                console.log(`Invalid input! Try again...`);
+                recursiveAsyncReadLine();
+            }
+        }
+        );
+    }
+    recursiveAsyncReadLine();
+}
+guessANumber()
